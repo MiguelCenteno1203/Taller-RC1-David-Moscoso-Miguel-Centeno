5a4a28cfcc1dfc21300dc1d664a3ca1b6396127e
MiguelCenteno1203 <mcentenom1203@gmail.com>
2026-05-25 15:53:44 -0500

    agregar funcion cargarCalificaciones con mapeo lineal de memoria

--- Taller-rc1.c
+++ Taller-rc1.c
@@ -5,6 +5,8 @@
 #define NOTA_MAXIMA 10.0
 #define NOTA_APROBATORIA 6.0
 
+void cargarCalificaciones(int n, float *ptr);
+
 int main() {
     int numEstudiantes;
     printf("==================================================\n");
@@ -22,5 +24,27 @@ int main() {
 
     float calificaciones[numEstudiantes][ASIGNATURAS];
 
+    // Llamada a la función de carga enviando el puntero base
+    cargarCalificaciones(numEstudiantes, (float *)calificaciones);
+
     return 0;
+}
+
+void cargarCalificaciones(int n, float *ptr) {
+    for (int i = 0; i < n; i++) {
+        printf("\n--- Registro - Estudiante %d ---\n", i + 1);
+        for (int j = 0; j < ASIGNATURAS; j++) {
+            float *nota = ptr + (i * ASIGNATURAS) + j; 
+            do {
+                printf("  Asignatura %d: ", j + 1);
+                if (scanf("%f", nota) != 1) {
+                    printf("    [!] Error de lectura. Reintente.\n");
+                    while (getchar() != '\n');
+                    *nota = -1.0;
+                } else if (*nota < NOTA_MINIMA || *nota > NOTA_MAXIMA) {
+                    printf("    [!] Rango invalido (%.1f - %.1f).\n", NOTA_MINIMA, NOTA_MAXIMA);
+                }
+            } while (*nota < NOTA_MINIMA || *nota > NOTA_MAXIMA);
+        }
+    }
 }
